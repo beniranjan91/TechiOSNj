9d2cbb4d91b7dd016a3f4d947a45d185ca21f0a6
Niranjan <niranjan.balasubramanian@target.com.au>
2023-01-20 14:22:04 +0530

    First commit

---
+++ FanOfMine/FanOfMineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FanOfMineApp.swift
+//  FanOfMine
+//
+//  Created by Niranjan on 20/01/23.
+//
+
+import SwiftUI
+
+@main
+struct FanOfMineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
